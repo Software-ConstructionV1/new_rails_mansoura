6c9ac82236fe1bb4a1bbeaf0402b512d175c6eed
Monica <monica@DESKTOP-2C083I0>
2025-05-09 19:50:52 +0300

    lab_one_a

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,6 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  get "/user" => "user#index"
+  get "/users" => "users#index"
   # root "posts#index"
 end
